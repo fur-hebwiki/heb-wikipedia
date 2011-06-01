63990d1aabad6b41f2f66b9c7ab62afddd849fc7
fur-hewiki <fur.hewiki@gmail.com>
2011-06-01 18:10:54 -0400

    do not belong here

--- stub.js
+++
@@ -1,5 +0,0 @@
-﻿stub_1 = "javascript:(function(){if(typeof(" 
-wikiit
-stub_2 = ")=='undefined'){var s=document.createElement('script');s.setAttribute('src','http://he.wikipedia.org/w/index.php?title= "
- משתמש:שמוליק/קישורים חיצוניים/chrome.js
-stub_3="&action=raw&ctype=text/javascript&dontcountme=s');document.getElementsByTagName('body')[0].appendChild(s);}else wikiit();})()"
