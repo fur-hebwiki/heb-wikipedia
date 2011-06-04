68fc6067beb868abb398f25a1576de86f3b3f169
fur-hewiki <fur.hewiki@gmail.com>
2011-06-04 10:24:53 -0400

    changed the way we read the list from wikized to raw, in order to handle beginning and ending spaces which wikization eats.

--- replaceBot.js
+++ replaceBot.js
@@ -1,6 +1,7 @@
 jsb_main = {
 	regexes: [],
 	skip_regexes: {},
+	badlines: [],
 	textbox: null,
 	build_regexes: function(event, data) {
 		var t = $('#wpTextbox1');
@@ -8,26 +9,35 @@ jsb_main = {
 		if (!this.textbox || this.textbox.value.length == 0 || /{{ללא_בוט}}/.test(this.textbox.value))
 			return;
 		if (data) {
-			var replace = $(data);
-			var trs = replace.find('tr');
-			var decoder = $('<div/>');
-			for (var i = 0; i < trs.length; i++) {
-				var tds = $(trs[i]).find('td');
-				if (tds.length > 4) {
+			var lines = data.split(/\n/);
+			while (lines.length) {
+				var line = lines.shift();
+				var matches = line.match(/^\|(\d+)/);
+				if (matches) {
+					var num = parseInt(matches[1], 10);
+					line = lines.shift();
+					line = line.replace(/<\/?nowiki>/g, '');
+					if (! line || ! line.length || line.substring(0,1) != '|')
+						continue;
+					line = line.substring(1).replace(/<\/?nowiki>/g, '');
 					try {
-						var regex = new RegExp(decoder.html(tds[1].innerHTML).text(), 'gi');
-						this.regexes.push([regex, decoder.html(tds[2].innerHTML).text()]);
-					} catch(ex) {
-						this.regexes.push('');
-						this.skip_regexes[i+1] = true;
+						var regex = new RegExp(line);
+					} catch(e) {
+						this.badlines.push([num, line]);
+						continue;
 					}
+					line = lines.shift();
+					if (! line || ! line.length || line.substring(0,1) != '|')
+						continue;
+					line = line.substring(1).replace(/<\/?nowiki>/g, '');
+					this.regexes[num] = [regex, line];
 				}
 			}
 			this.process_page();
 		}
 		else 
 			$.ajax({
-				url: wgServer + wgScriptPath + '/index.php?title=ויקיפדיה:בוט/בוט החלפות/רשימת החלפות נוכחית',
+				url: wgServer + wgScriptPath + '/index.php?title=ויקיפדיה:בוט/בוט החלפות/רשימת החלפות נוכחית&action=raw&ctype=text/x-wiki',
 				context: document.body,
 				success: function(data, status, jqhxr){
 					jsb_main.build_regexes(null, data);
@@ -41,8 +51,8 @@ jsb_main = {
 		if (skipmatch)
 			for (var i = 1; i < skipmatch.length; i++)
 				this.skip_regexes[parseInt(skipmatch[i], 10)] = true;
-		for (var i = 0; i < this.regexes.length; i++) 
-			if (!this.skip_regexes[i+1])
+		for (var i in this.regexes) 
+			if (!this.skip_regexes[i])
 				t = t.replace(this.regexes[i][0], this.regexes[i][1]);
 		this.textbox.value = t;
 		alert('סקריפט בוט החלפות סיים לרוץ. אנא בצעו "הצגת שינויים" לפני שמירה, כדי לוודא שהסקריפט לא גרם נזק');
