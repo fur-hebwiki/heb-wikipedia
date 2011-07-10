f2caab9f7c6ca860dbaea825e85a3621dbe2ca12
fur-hewiki <fur.hewiki@gmail.com>
2011-07-10 14:08:44 -0400

    almost done

--- imageHighlight.js
+++ imageHighlight.js
@@ -1,71 +1,80 @@
-function clearCanvas(area) {
-	var context = area.canvasContext;
-	context.clearRect(0,0,context.canvas.width, context.canvas.height);
-}
-
-function drawMarker(area) {
-	var context = area.canvasContext;
-	var coords = area.coords.split(',');
-	
-	function drawPoly() {
-		context.beginPath();
-		context.moveTo(coords.shift(), coords.shift());
-		while (coords.length) 
-			context.lineTo(coords.shift(), coords.shift());
-		context.closePath();
-		context.stroke();
-		context.fill();
-	}
-	
-	function drawRect() {
-		context.strokeRect(coords[0],coords[1],coords[2]-coords[0],coords[3]-coords[1]);
-		context.fillRect(coords[0],coords[1],coords[2]-coords[0],coords[3]-coords[1]);
-	}
-	
-	function drawCircle() {
-		context.beginPath();
-		context.arc(coords[0],coords[1],coords[2],0,Math.PI*2,true);
-		context.closePath();
-		context.stroke();
-		context.fill();
-	}
-	
-	context.fillStyle = 'rgba(0,0,0,0.4)';
-	context.strokeStyle = 'yellow';
-	context.lineWidth = 6;
-	switch (area.shape) {
-		case 'rect': drawRect(); break;
-		case 'circle': drawCircle(); break;
-		case 'poly': drawPoly(); break;
-	}
-	
-	//alert(area.shape + '\n' + area.coords + '\n' + area.title);
-}
-
 function mah_init() {
-	var imgs = $(".img_toggle").find('img');
+	
+	function clearCanvas(area) {
+		var context = area.canvasContext;
+		context.clearRect(0,0,context.canvas.width, context.canvas.height);
+	}
+
+	function drawMarker(area) {
+		var context = area.canvasContext;
+		var coords = area.coords.split(',');
+		
+		function drawPoly() {
+			context.beginPath();
+			context.moveTo(coords.shift(), coords.shift());
+			while (coords.length) 
+				context.lineTo(coords.shift(), coords.shift());
+			context.closePath();
+			context.stroke();
+			context.fill();
+		}
+		
+		function drawRect() {
+			context.strokeRect(coords[0],coords[1],coords[2]-coords[0],coords[3]-coords[1]);
+			context.fillRect(coords[0],coords[1],coords[2]-coords[0],coords[3]-coords[1]);
+		}
+		
+		function drawCircle() {
+			context.beginPath();
+			context.arc(coords[0],coords[1],coords[2],0,Math.PI*2,true);
+			context.closePath();
+			context.stroke();
+			context.fill();
+		}
+		
+		context.fillStyle = 'rgba(0,0,0,0.4)';
+		context.strokeStyle = 'yellow';
+		context.lineWidth = 6;
+		switch (area.shape) {
+			case 'rect': drawRect(); break;
+			case 'circle': drawCircle(); break;
+			case 'poly': drawPoly(); break;
+		}
+	}
+	
+	
+	
+	
+	
+	var imgs = $(".imagetest").find('img');
 	for (var imgnum = 0; imgnum < imgs.length; imgnum++) {
 		img = $(imgs[imgnum]);
 		parent = img.parent();
+		parent.css('zIndex', 10);
 		var bgimg = $('<img>');
 		var width = img.width(), height = img.height();
 		var dims = {position: 'absolute', width: width + 'px', height: height + 'px', top: img.position().top + 'px', left: img.position().left + 'px', border: 0}
 		bgimg.css(dims);
 		bgimg.attr('src', img.attr('src'));
-		parent.css('zIndex', 10);
 		img.before(bgimg);
 		img.fadeTo(5, 0.4);
 		var jcanvas = $('<canvas>');
 		jcanvas.css(dims);
 		img.before(jcanvas);
 		var canvas = jcanvas[0];
+		canvas.height = height; canvas.width = width;
 		var context = canvas.getContext("2d");
 		var ol = $('<ol>');
-		ol.css('margin', '30px');
+		ol.css({clear: 'both'});
 		parent.after(ol);
+		var div = $('<div>'); div.css('clear', 'both');
+		ol.after(div);
+		div.after('<hr>');
+		
 		var areas = parent.find('area');
 		for (var i = 0; i < areas.length; i++) {
 			var li = $('<li>');
+			li.css({float: 'right', marginLeft: '2em', marginRight: '2em', whiteSpace: 'nowrap'});
 			li[0]['area'] = areas[i]
 			$.extend(areas[i], {li: li, canvasContext: context});
 			li.html('<a href="'+areas[i].href+'">' + areas[i].title + '</a>');
