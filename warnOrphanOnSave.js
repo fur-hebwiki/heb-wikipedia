53f87515cde44cb130aeb34be99fdd94672f3ad8
fur-hebwiki <fur.hebwiki@gmail.com>
2011-07-19 18:28:38 -0400

    ignore redirects

--- warnOrphanOnSave.js
+++ warnOrphanOnSave.js
@@ -1,6 +1,9 @@
 ﻿if (wgNamespaceNumber == 0 && getParamValue('action') == 'edit') 
 	$.getJSON(wgScriptPath + '/api.php?', {format: 'json', action: 'query', list: 'backlinks', bltitle: wgTitle, blnamespace: 0}, 
 		function(data) {
-			if (data && data.query && typeof data.query.backlinks != "undefined" && data.query.backlinks.length == 0) 
-				$('#wpSave').click(function(){alert('הערך ' + wgTitle + ' "יתום": אין ערך אחר המקשר אליו.\nאנא נסו למצוא ערכים קיימים שיכולים לקשר לערך זה, ולהוסיף קישורים כאלו.');});
+			if (data && data.query && typeof data.query.backlinks != "undefined" && data.query.backlinks.length == 0)
+				$('#wpSave').click(function(){
+					if ($('#wpTextbox1').text().indexOf('#הפניה'))
+						alert('הערך ' + wgTitle + ' "יתום": אין ערך אחר המקשר אליו.\nאנא נסו למצוא ערכים קיימים שיכולים לקשר לערך זה, ולהוסיף קישורים כאלו.');
+				});
 		});
