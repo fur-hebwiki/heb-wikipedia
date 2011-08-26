488711216f57fc2d08926f3d2f2ef606c611bd2d
shmulikk <sking.me@gmail.com>
2011-08-26 12:26:07 +0300

    alert when clicked

--- bookmarkletsinit.js
+++ bookmarkletsinit.js
@@ -1,30 +1,35 @@
 function create_bookmarklets() {
 	var stubparts = [
 		"javascript:(function(){if(typeof(", 
-		")=='undefined'){var s=document.createElement('script');s.setAttribute('src','http://he.wikipedia.org/w/index.php?title=",
+		")=='undefined'){var s=document.createElement('script');s.setAttribute('src','http://he.wikipedia.org/w/index.php?title= ",
 		"&action=raw&ctype=text/javascript&dontcountme=s');document.getElementsByTagName('body')[0].appendChild(s);}else ",
 		"();})()"
 	];
 	var bookmarklets = [
-		{func: 'wikiit', sourcepage: 'משתמש:שמוליק/קישורים חיצוניים/chrome.js', name: 'צור תבניות לויקי', description: 'יצירת תבנית קישור ממאמרים ב"הארץ", נרג, טמקא ועוד: '},
-		{func: 'coords', sourcepage: 'משתמש:קיפודנחש/common.js/coords.js', name: 'מציאת קואורדינטות', description: 'יצירת תבנית coord ממפה של גוגל מפות או עמוד ענן: '},
+		{func: 'wikiit', sourcepage: 'משתמש:שמוליק/קישורים חיצוניים/chrome.js'  , name: 'תבנית לויקיפדיה', description: 'יצירת תבנית קישור ממאמרים ב"הארץ", nrg, ynet ועוד: '},
+		{func: 'coords', sourcepage: 'משתמש:קיפודנחש/common.js/coords.js', name: 'תבנית coord', description: 'יצירת תבנית coord ממפה של גוגל או עמוד ענן (ראו הוראות למטה): '}
 	];
+	var $ol = $("#bmList ol:first");
+	$ol.contents().remove();
+	$(bookmarklets).each(function(idx, item){
+		$("<li>").text(item.description)
+		.append(
+			$("<a>",
+				{
+				"href": stubparts[0] + item.func + stubparts[1]+ item.sourcepage + stubparts[2] + item.func + stubparts[3],
+				"click": bookmarlets_wrong,
+				"text" : item.name
+				}
+			)
+		).appendTo($ol)
+	});
 	
-	var lists = document.getElementById('bmList').getElementsByTagName('ol'),
-		list = lists[0];
-	while (list.firstChild)
-		list.removeChild(list.firstChild);
-	for (var bmi in bookmarklets) {
-		var bm = bookmarklets[bmi];
-		var li = document.createElement('li');
-		var a = document.createElement('a');
-		a.href = stubparts[0] + bm.func + stubparts[1] + bm.sourcepage + stubparts[2] + bm.func + stubparts[3];
-		a.innerHTML = bm.name;
-		li.appendChild(document.createTextNode(bm.description));
-		li.appendChild(a);
-		list.appendChild(li);
+	function bookmarlets_wrong(e)
+	{
+		e.preventDefault();
+		alert('את הקישור יש לגרור לסרגל המועדפים, ולהפעיל באתרים המתאימים.\nקישור זה לא מיועד להפעלה מתוך ויקיפדיה. למידע נוסף קרא את ההוראות בדף.');
 	}
-}
+	
+};
 
-if (wgPageName == 'משתמש:קיפודנחש/בוקמרקלטים')
-	addOnloadHook(create_bookmarklets);
+addOnloadHook(create_bookmarklets);
