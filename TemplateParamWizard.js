0bfffe7463a304b5d4364936236e3e799d088f28
fur-hewiki <fur.hewiki@gmail.com>
2011-11-27 18:03:42 -0500

    almost working now

--- TemplateParamWizard.js
+++ TemplateParamWizard.js
@@ -14,20 +14,19 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 	// attribs: bool optional, 
 	// options: array of possible values - undefined if it's a free text param
 	// templateParams is keyed by param name.
-	var templateParams = {};
-	
+	var templateParams,
 	// which template are we working on
-	var template;
-	
+		template,
 	// the input fileds. each field has some data also, e.g., the param name.
-	var dialogFields = [];
+		dialogFields;
 	
 	function paramsFromSelection() {
 	}
 	
 	function buildParams(data) {
+		templateParams = {};
 		var lines = data.split("\n");
-		while (lines) {
+		while (lines && lines.length) {
 			var line = lines.shift();
 				if (!(/^\|-/.test(line))) // look for |- this is wikitext for table row.
 					continue;
@@ -47,7 +46,7 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 			var pAttribs = {desc: desc};
 			if (required)
 				pAttribs.required = true;
-			if (fileds.length > 2)
+			if (fields.length > 2)
 				pAttribs.defVal = $.trim(fields[2]);
 			if (fields.length > 3)
 				pAttribs.options = fileds[3].split(",");
@@ -57,14 +56,15 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 	
 	function createWikiCode() {
 		var par = [template];
-		for (j in dialogFields) {
+		for (var i in dialogFields) {
 			var 
 				field = dialogFields[i],
+				name = $.trim(field[0]),
 				val = $.trim(field[1].val()).replace(/\|/g, '{{!}}');
 			if (val.length)
-				par.push(field.data('paramName') + '=' + val);
+				par.push(name + '=' + val);
 		}
-		return "{{" + par.join("\n|") + "}}";
+		return "{{" + par.join("\n|") + "\n}}";
 	}
 
 	function showPreview() {
@@ -94,29 +94,14 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 	}
 	
 	var brainDamage = $.browser.msie && $.browser.version < 8;
-	var	table = $('<table>');
 
 
 	function updateRawPreview(){
-		$('#tpw_preview').text(createWikiCode());
-		var canOK = 'enable';
-		var f = orderedFields.concat([]); //clone - we don't want to touch orderedFields.
-		for (var i in namedFields)
-			f.push(namedFields[i][1]);
-		for (var i in f)
-			if (f[i].hasClass('tpw_required') && $.trim(f[i].val()).length == 0)
-				canOK = 'disable';
-		$(".ui-dialog-buttonpane button:contains('אישור')").button(canOK);
-		if (brainDamage) { //IOW: internet explorer.
-			var width = $('#tpw_dialog').width() - 12;
-			$('.ui-dialog-titlebar').width(width);
-			$('.ui-dialog-buttonpane').width(width);
-		}
 	}
 	
 	function toggleDesc() {$(this).next('span').toggleClass('hiddenDesc');}
 	
-	function addRow(paramName) {
+	function addRow(paramName, table) {
 		var templateParam = templateParams[paramName];
 		var options = templateParam.options;
 		var inputField = options 
@@ -150,22 +135,27 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 		dialogFields.push([paramName, inputField]);
 		table.append(tr);
 	}
+	
+	function injectResults() {
+		$("#wpTextbox1").textSelection('encapsulateSelection', {replace: true, peri: createWikiCode()});
+	}
+	
 	function buildDialog(data) {
-		$('.tpw_disposable').remove();
-		buildParams(data);		
-		dialog
+		buildParams(data);
+		var	table = $('<table>');
+		var dialog = $('<div>', {'class': 'tpw_disposable'}).dialog()
 			.append($('<p>').text(i18n('explainOptional')))
 			.append(table)
 			.append($('<p>').css({height: '2em'}))
 			.append($('<div>', {id: 'tpw_preview'})
-					.css({backgroundColor: "lightGreen", maxWidth: '40em', maxHeight: '8em', overflow: 'auto'}));
+				.css({backgroundColor: "lightGreen", maxWidth: '40em', maxHeight: '8em', overflow: 'auto'}));
 
-
+		dialogFields = [];
 		for (var paramName in templateParams)
-			addRow(paramName);
+			addRow(paramName, table);
 		
 		var buttons = {}; // we need to do it this way, because with literal object, the keys must be literal.
-		buttons[i18n('ok')] = function() {insertTags('', createWikiCode(), ''); dialog.dialog('close'); };
+		buttons[i18n('ok')] = function() {injectResults(); dialog.dialog('close'); };
 		buttons[i18n('cancel')] = function() {dialog.dialog('close');}
 		buttons[i18n('preview')] = showPreview;
 		dialog.dialog('option', 'buttons', buttons);
@@ -182,7 +172,7 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 
 	function fireDialog() {
 		if (!template) {
-			var match = $("#wpTextbox1").textSelection('getSelection').match(/^\{\{([^|]*)/);
+			var match = $("#wpTextbox1").textSelection('getSelection').match(/^\{\{([^|}]*)/);
 			if (match)
 				template = $.trim(match[1]);
 		}
@@ -191,7 +181,7 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 		
 		$.ajax({
 			url: mw.util.wikiScript('index'),
-			contents: {title: param_page(), action: 'raw', ctype: 'text/x-wiki'},
+			data: {title: paramPage(), action: 'raw', ctype: 'text/x-wiki'},
 			success: buildDialog
 		});
 	}
@@ -201,169 +191,29 @@ mw.loader.using(['jquery.ui.widget','jquery.ui.autocomplete','jquery.textSelecti
 		"</style> "
 	).appendTo("head");
 
-	var mode="none";
-	var templateParamsNotFound = false;
-	function findLinks(res){
-		var pos=$( "#wpTextbox1" ).textSelection('getCaretPosition')-1;
-		var txt=$( "#wpTextbox1" ).val();
-
-		var lastbegin=txt.lastIndexOf("[[",pos);
-		var lastend=txt.lastIndexOf("]]",pos);
-		var isLink=lastbegin>lastend;
-		if(isLink) {
-			fillLinksList(res,txt.substr(lastbegin+2,pos-lastbegin));
-			mode='link';
-		}
-		else{
-			lastbegin=txt.lastIndexOf("{{",pos);
-			lastend=txt.lastIndexOf("}}",pos);
-			var isTemplate=lastbegin>lastend;
-			if(isTemplate){
-				mode='template';
-				if (txt.substr(pos, 1) == '|') { // in template mode, | is as good as enter
-					addAutocompleteLink(txt.substr(lastbegin+2,pos-lastbegin))
-					return;
+	setTimeout(function() {
+		var buttonImage = '//upload.wikimedia.org/wikipedia/he/math/b/e/f/bef83e1ceb68569ed581318e6108e8a2.png';
+		if (typeof $.wikiEditor != 'undefined')
+			$('#wpTextbox1').wikiEditor('addToToolbar', {
+				section: 'advanced',
+				group: 'more',
+				tools: {
+					'linkTemplatewizard': {
+						label: 'תבנית קישור',
+						type: 'button',
+						icon: buttonImage,
+						action: {type: 'callback', execute: fireDialog}
+					}
 				}
-				var prefixName=mw.config.get('wgFormattedNamespaces')[10]+':'+txt.substr(lastbegin+2,pos-lastbegin);
-				fillLinksList(res,prefixName);
-			}
-			else{
-				res([]);
-				mode="none";
-			}
-		}
-	}
-
-	function fillLinksList(res,txt){
-		if(txt.length<=1 || txt.indexOf('|')>-1 || txt.indexOf('#')>-1) res([]);
-		else $.getJSON(mw.util.wikiScript('api'),{action:'opensearch',search:txt,format:'json'},function(data){if(data[1]) res(data[1]);});
-	}
-	
-	function addAutocompleteLink(item){
-		switch(mode){
-		case "none": return;
-		case "template":
-			if (templateParamsNotFound) {
-				item = item.substr(mw.config.get('wgFormattedNamespaces')[10].length+1);
-				break;
-			}
-			else {
-				template = item;
-				$.ajax({
-					url: mw.util.wikiScript('index'),
-					data: {
-						title: paramPage(),
-						action: 'raw',
-						ctype: 'text/x-wiki'
-					},
-					success: buildDialog,
-					error: function() {
-						templateParamsNotFound = true;
-						addAutocompleteLink(item);
-					}
-				});
-				return;
-			}
-		}
-		if(mode=="none") return;
-		var pos=$( "#wpTextbox1" ).textSelection('getCaretPosition')-1;
-		var txt=$( "#wpTextbox1" ).val();
-		var lastbegin=txt.lastIndexOf((mode=="link"?"[[":"{{"),pos);
-		
-		var newTxt=txt.substr(0,lastbegin)+(mode=="link"?"[[":"{{")+item+(mode=="link"?"]]":"}}")+txt.substr(pos+1);
-		$( "#wpTextbox1" ).val(newTxt);
-		$( "#wpTextbox1" ).textSelection('setSelection',{start:lastbegin+item.length+4});
-	}
-
-	$( "#wpTextbox1" ).autocomplete({
-		source: function( request, response ) {
-			findLinks(response);
-			fixArrowsBug(this);
-		},
-		focus:function(){return false;},
-		select:function(e,ui) {
-			addAutocompleteLink(ui.item.value);return false;
-		},
-		open:function(){
-			$(".ui-autocomplete").css('width','400px');
-			$(".ui-autocomplete").position({
-				my: $('body').is('.rtl')? "left bottom" : "right bottom",
-				at: $('body').is('.rtl')? "left bottom" : "right bottom",
-				of: "#wpTextbox1"
 			});
-		} 
-	});
-	
-	var fixed=false;
-	//this is hack to prevent known serious bug in autocomplete.js that prevent default of the up and down key which may drive you crazy....
-	function fixArrowsBug(self) {
-		if (fixed) 
-			return;
-		fixed=true;
-		$("#wpTextbox1")
-		.unbind("keydown.autocomplete")
-		.bind("keydown.autocomplete", function(event ) {
-			switch (event.keyCode) {
-				case keyCode.PAGE_UP:
-					self._move( "previousPage", event );
-					break;
-				case keyCode.PAGE_DOWN:
-					self._move( "nextPage", event );
-					break;
-				case keyCode.UP:
-					if (!self.menu.element.is(":visible")) 
-						return;
-					self._move( "previous", event );
-					// prevent moving cursor to beginning of text field in some browsers
-					event.preventDefault();
-					break;
-				case keyCode.DOWN:
-					if (!self.menu.element.is(":visible")) 
-						return;
-					self._move( "next", event );
-					// prevent moving cursor to end of text field in some browsers
-					event.preventDefault();
-					break;
-				case keyCode.ENTER:
-				case keyCode.NUMPAD_ENTER:
-					// when menu is open or has focus
-					if ( self.menu.active ) {
-						event.preventDefault();
-					}
-					//passthrough - ENTER and TAB both select the current element
-				case keyCode.TAB:
-					if ( !self.menu.active ) {
-						return;
-					}
-					self.menu.select( event );
-					break;
-				case keyCode.ESCAPE:
-					self.element.val( self.term );
-					self.close( event );
-					break;
-				case keyCode.LEFT:
-				case keyCode.RIGHT:
-				case keyCode.SHIFT:
-				case keyCode.CONTROL:
-				case keyCode.ALT:
-				case keyCode.COMMAND:
-				case keyCode.COMMAND_RIGHT:
-				case keyCode.INSERT:
-				case keyCode.CAPS_LOCK:
-				case keyCode.END:
-				case keyCode.HOME:
-					// ignore metakeys (shift, ctrl, alt)
-					break;
-				default:
-					// keypress is triggered before the input value is changed
-					clearTimeout( self.searching );
-					self.searching = setTimeout(function() {
-						self.search( null, event );
-					}, self.options.delay );
-					break;
-			}
-		});
-	}
+		else
+			$('div #toolbar').append( // "old style"
+				$('<img>', {src: buttonImage, title: 'תבנית קישור', 'class': 'mw-toolbar-editbutton'})
+				.css({cursor: 'pointer'})
+				.click(fireDialog)
+			);
+			
+	}, 120);
 
 });
 });
