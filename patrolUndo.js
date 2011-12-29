fd952bf47fd42e2331e372371075ce2dca82b5ce
fur-hebwiki <fur.hebwiki@gmail.com>
2011-12-29 13:56:03 -0500

    adding a newone: mark edit as 'patrolled' on undo (aka 'bitul') also, not just rollback

---
+++ patrolUndo.js
@@ -0,0 +1,25 @@
+﻿if (wgAction == 'history') 
+$(document).ready(function() {
+	$('.mw-history-undo').click(function() {
+		var href=$(this).find('a:eq(0)').attr('href');
+		if (!href)
+			return;
+		var user = $(this).siblings('.history-user').find('a:eq(0)').text();
+		if (!user)
+			return;
+		var match = href.match(/&undo=(\d*)/);
+		if (!match.length)
+			return;
+		var revToMark = parseInt(match[1], 10);
+		$.ajaxSetup({url: mw.util.wikiScript('api'), type: 'post', async: false});
+		$.ajax({
+			data: {action: 'query', list: 'recentchanges', rctoken: 'patrol', rcprop: 'ids|patrolled', rclimit: 500, rcuser: user, format: 'json'},
+			success: function(data) {
+				var item = $.grep(data.query.recentchanges, function(i){return i.revid == revToMark}).pop();
+				if (!item || !item.rcid || !item.patroltoken || (typeof item.patrolled == "string"))
+					return;
+				$.ajax({data: {action: 'patrol', rcid: item.rcid, token: item.patroltoken, format: 'json'}});
+			} 
+		});
+	});
+});
