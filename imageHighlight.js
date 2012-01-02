4ab18688de773137c451308a8c94b37ec9f00541
fur-hewiki <fur.hewiki@gmail.com>
2012-01-02 10:06:55 -0500

    it's considered to be impolite to call a class member 'class' and not use quotes when addressing it. specifically, ie doesn't like it.

--- imageHighlight.js
+++ imageHighlight.js
@@ -11,7 +11,7 @@ function mah_init() {
 		liHighlighting = {background: 'yellow'},
 //css for un-highlighting li element:
 		liBackToNormal = {background: ''},
-//every imagemap that wants highlighting, should reside in a div of this class:
+//every imagemap that wants highlighting, should reside in a div of this 'class':
 		hilightDivMarker = '.imagetest';
 
 	function drawMarker(context, areas) {
@@ -58,18 +58,18 @@ function mah_init() {
 			img.fadeTo(1, 0);	//make the image transparent - it is still imagemap, but we see bgimg through it.
 			var dims = {position: 'absolute', width: img.width() + 'px', height: img.height() + 'px',
 						top: img.position().top + 'px', left: img.position().left + 'px', border: 0};
-			var bgimg = $('<img>', {class: myClassName, src: img.attr('src')}).css(dims);//completely inert image. this is what we see.
-			var jcanvas = $('<canvas>', {class: myClassName}).css(dims).attr({width: img.width(), height: img.height()});
+			var bgimg = $('<img>', {'class': myClassName, src: img.attr('src')}).css(dims);//completely inert image. this is what we see.
+			var jcanvas = $('<canvas>', {'class': myClassName}).css(dims).attr({width: img.width(), height: img.height()});
 			img.before(bgimg).before(jcanvas);	//both canvas and bgimage are behind the transparent image/imagemap
 			context = $.extend(jcanvas[0].getContext("2d"), areaHighLighting);
 		}
-		var ol = $('<ol>', {class: myClassName}).css({clear: 'both', marginTop: '1.5em', width: img.width()});
-		img.after($('<hr>', {class: myClassName}).css('clear', 'both')).after(ol); //ol below image, hr below ol. for thumbs, the caption appears below the hr.
+		var ol = $('<ol>', {'class': myClassName}).css({clear: 'both', marginTop: '1.5em', width: img.width()});
+		img.after($('<hr>', {'class': myClassName}).css('clear', 'both')).after(ol); //ol below image, hr below ol. for thumbs, the caption appears below the hr.
 		var lis = {};	//collapse areas with same caption to one list item
 		img.parent().find('area').each(function() {
 			var li = lis[this.title];	//saw it previously? use the same li
 			if (!li) {	//no? create a new one.
-				lis[this.title] = li = $('<li>', {class: myClassName})
+				lis[this.title] = li = $('<li>', {'class': myClassName})
 					.css(liElementCss)
 					.append($('<a>', {href: this.href, text: this.title})) //put <a> with link and caption inside it
 					.mouseover(highlight)
@@ -87,5 +87,5 @@ function mah_init() {
 	});
 }
 
-addOnloadHook(mah_init);
+$(document).ready(mah_init);
 $(window).resize(mah_init);
