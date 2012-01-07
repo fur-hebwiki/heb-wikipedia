4a1794e27e5b9de0867a9f84d19856cb3431610b
fur-hewiki <fur.hewiki@gmail.com>
2012-01-07 16:52:19 -0500

    signal success by removing linkette

--- askOTRS.js
+++ askOTRS.js
@@ -12,7 +12,8 @@ $(document).ready(function() {
 				var message = '\n\n==[[:' + wgPageName + ']]==\n{{אישור OTRS|תמונה=' + wgTitle + '}} ~~' + '~~';
 				var summary = 'בקשת אישור OTRS ל[[:' + wgPageName + ']]';
 				var param = {action: 'edit', title: talkPage, summary: summary, token: mw.user.tokens.get('editToken'), appendtext: message, format: 'json'};
-				$.post(mw.util.wikiScript('api'), param, function(){alert('הבקשה לאישור OTRS התווספה לדף המשתמש - ' + talkPage);}); //flying blind
+				var $this = $(this);
+				$.post(mw.util.wikiScript('api'), param, function(){$this.remove();});
 			})
 		)
 		.after(' | ')
