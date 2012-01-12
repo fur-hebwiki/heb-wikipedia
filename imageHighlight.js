b78b40b3b7fe0bb02b49c9c4290e9ca62e4b98c9
fur-hebwiki <fur.hebwiki@gmail.com>
2012-01-12 15:43:54 -0500

    it's all about positioning: instead of making calculations which will later be irrelevant, wrap it all tightly in a div, and then the offsets can be 0.

--- imageHighlight.js
+++ imageHighlight.js
@@ -54,21 +54,27 @@ $(document).ready(function() {
 	function mah_init() {
 		$('.' + myClassName).remove(); //remove artefacts (if any) from previous run.
 		$(hilightDivMarker).find('img').each(function() {
-			var img = $(this);
-			if (!img.parent().find('area').length)
+			var img = $(this), map = img.siblings('map:first');
+			if (!map.find('area').length)
 				return;	//not an imagemap. inside "each" anonymous function, 'return' means "continue".
 			img.fadeTo(1, 0);	//make the image transparent - it is still imagemap, but we see bgimg through it.
-			var dims = {position: 'absolute', width: img.width() + 'px', height: img.height() + 'px',
-						top: img.position().top + 'px', left: img.position().left + 'px', border: 0};
-			var bgimg = $('<img>', {'class': myClassName, src: img.attr('src')}).css(dims);//completely inert image. this is what we see.
-			var jcanvas = $('<canvas>', {'class': myClassName}).css(dims).attr({width: img.width(), height: img.height()});
+			var w = img.width(), h = img.height();
+			var dims = {position: 'absolute', width: w + 'px', height: h + 'px', border: 0, top:0, left:0};
+			var jcanvas = $('<canvas>', {'class': myClassName})
+				.css(dims)
+				.attr({width: w, height: h});
+			var bgimg = $('<img>', {'class': myClassName, src: img.attr('src')})
+				.css(dims);//completely inert image. this is what we see.
 			var context = $.extend(jcanvas[0].getContext("2d"), areaHighLighting);
-			
-			img.before(bgimg).before(jcanvas);	//both canvas and bgimage are behind the transparent image/imagemap
+			var div = $('<div>').css({position: 'relative', width: w + 'px', height: h + 'px'});
+			img.before(div);	// put the div just above the image, and ...
+			div.append(bgimg)	// place the background image in the div
+				.append(jcanvas)// and the canvas,
+				.append(img);	// now yank the image from the window and place it in the div also.
 			var ol = $('<ol>', {'class': myClassName}).css({clear: 'both', marginTop: '1.5em'});
-			img.after($('<hr>', {'class': myClassName}).css('clear', 'both')).after(ol); //ol below image, hr below ol. for thumbs, the caption appears below the hr.
+			div.after($('<hr>', {'class': myClassName}).css('clear', 'both')).after(ol); //ol below image, hr below ol. for thumbs, the caption appears below the hr.
 			var lis = {};	//collapse areas with same caption to one list item
-			img.parent().find('area').each(function() {
+			map.find('area').each(function() {
 				var li = lis[this.title];	//saw it previously? use the same li
 				if (!li) {	//no? create a new one.
 					lis[this.title] = li = $('<li>', {'class': myClassName})
@@ -94,7 +100,5 @@ $(document).ready(function() {
 					'.ltr li.' + myClassName + '{float: right; margin-right: 3em;}'
 					); 
 		mah_init();
-		$('.a_toggle').live('click', mah_init); // specific for hewiki.
-		$(window).resize(mah_init);
 	}	
 });
